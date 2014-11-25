d7635a6bc89f13742bf692660b8b8436ee69f0f3
Gordon Lesti <info@gordonlesti.com>
2014-11-25 20:40:58 +0100

    get with tests

--- src/NumPHP/Core/NumArray.php
+++ src/NumPHP/Core/NumArray.php
@@ -33,7 +33,7 @@ class NumArray
     {
         $shape = [];
         $this->array = $array;
-        $this->shape = $this->initRecursive($array, $shape);
+        $this->shape = self::initRecursive($array, $shape);
     }
 
     /**
@@ -45,12 +45,68 @@ class NumArray
     }
 
     /**
+     * @return mixed|NumArray
+     */
+    public function get()
+    {
+        $args = func_get_args();
+        $array = self::getRecursive($this->array, $args);
+
+        if (is_array($array)) {
+            return new NumArray($array);
+        }
+        return $array;
+    }
+
+    /**
+     * @param $array
+     * @param array $args
+     * @return mixed
+     */
+    protected static function getRecursive($array, array $args)
+    {
+        if (isset($args[0])) {
+            $arg = $args[0];
+            $matches = [];
+            array_shift($args);
+            if (preg_match('/^(?P<from>\d*):(?P<to>\d*)$/', $arg, $matches)) {
+                $fromValue = $matches['from'];
+                $toValue = $matches['to'];
+                $sliced = self::slice($array, $fromValue, $toValue);
+                foreach ($sliced as $index => $row) {
+                    $sliced[$index] = self::getRecursive($row, $args);
+                }
+                return $sliced;
+            }
+            return self::getRecursive($array[$arg], $args);
+        }
+        return $array;
+    }
+
+    /**
+     * @param $array
+     * @param $fromValue
+     * @param $toValue
+     * @return array
+     */
+    protected static function slice(array $array, $fromValue, $toValue)
+    {
+        if (!$fromValue) {
+            $fromValue = 0;
+        }
+        if (!$toValue && $toValue !== 0) {
+            $toValue = count($array);
+        }
+        return array_slice($array, $fromValue, $toValue-$fromValue);
+    }
+
+    /**
      * @param $array
      * @param $shape
      * @param int $level
      * @return mixed
      */
-    protected function initRecursive($array, $shape, $level = 0)
+    protected static function initRecursive($array, $shape, $level = 0)
     {
         if (is_array($array)) {
             $count = count($array);
@@ -60,7 +116,7 @@ class NumArray
                 $shape[$level] = $count;
             }
             foreach ($array as $row) {
-                $shape = $this->initRecursive($row, $shape, $level+1);
+                $shape = self::initRecursive($row, $shape, $level+1);
             }
         }
         return $shape;
