03c4447c13ef2e28ed8bf4749dd3f0bbf7de5f11
Gordon Lesti <info@gordonlesti.com>
2014-12-16 09:38:05 +0100

    add version and a test

---
+++ src/NumPHP/Core/Version.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * NumPHP (http://numphp.org/)
+ *
+ * @link http://github.com/GordonLesti/NumPHP for the canonical source repository
+ * @copyright Copyright (c) 2014 Gordon Lesti (http://gordonlesti.com/)
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace NumPHP\Core;
+
+/**
+ * Class Version
+  * @package NumPHP\Core
+  */
+class Version
+{
+    const VERSION = '1.0.0-dev';
+}
