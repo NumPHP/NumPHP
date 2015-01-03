0efaeb9fdd8b3ebc393334b5812ddf51a740f338
Gordon Lesti <info@gordonlesti.com>
2015-01-04 00:20:54 +0100

    update StringTest

--- tests/NumPHPTest/Core/NumArray/StringTest.php
+++ tests/NumPHPTest/Core/NumArray/StringTest.php
@@ -21,21 +21,21 @@ class StringTest extends \PHPUnit_Framework_TestCase
     public function testToString()
     {
         $numArray = new NumArray(1);
-        $this->expectOutputString("NumArray(1)");
+        $this->expectOutputString("NumArray(1)\n");
         echo $numArray;
     }
 
     public function testToString2()
     {
         $numArray = NumPHP::arange(1, 2);
-        $this->expectOutputString("NumArray([1, 2])");
+        $this->expectOutputString("NumArray([1, 2])\n");
         echo $numArray;
     }
 
     public function testToString3x4()
     {
         $numArray = NumPHP::arange(1, 12)->reshape(3, 4);
-        $expectedOutput = "NumArray([\n  [1, 2, 3, 4],\n  [5, 6, 7, 8],\n  [9, 10, 11, 12]\n])";
+        $expectedOutput = "NumArray([\n  [1, 2, 3, 4],\n  [5, 6, 7, 8],\n  [9, 10, 11, 12]\n])\n";
         $this->expectOutputString($expectedOutput);
         echo $numArray;
     }
