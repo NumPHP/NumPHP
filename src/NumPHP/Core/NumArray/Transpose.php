6b31a227ba2192a946b129e53a475ab67fedad32
Gordon Lesti <info@gordonlesti.com>
2015-01-08 15:07:56 +0100

    phpcs

--- src/NumPHP/Core/NumArray/Transpose.php
+++ src/NumPHP/Core/NumArray/Transpose.php
@@ -2,25 +2,38 @@
 /**
  * NumPHP (http://numphp.org/)
  *
- * @link http://github.com/GordonLesti/NumPHP for the canonical source repository
- * @copyright Copyright (c) 2014 Gordon Lesti (http://gordonlesti.com/)
- * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * PHP version 5
+ *
+ * @category  Core
+ * @package   NumPHP\Core\NumArray
+ * @author    Gordon Lesti <info@gordonlesti.com>
+ * @copyright 2014-2015 Gordon Lesti (https://gordonlesti.com/)
+ * @license   http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link      http://numphp.org/
  */
 
 namespace NumPHP\Core\NumArray;
 
 /**
  * Class Transpose
- * @package NumPHP\Core\NumArray
+ *
+ * @category Core
+ * @package  NumPHP\Core\NumArray
+ * @author   Gordon Lesti <info@gordonlesti.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     http://numphp.org/
  */
 class Transpose
 {
     const CACHE_KEY_TRANSPOSE = 'transpose';
 
     /**
-     * @param $data
-     * @param array $shape
-     * @return array|mixed|\NumPHP\Core\NumArray
+     * Creates a transposed value or array
+     *
+     * @param mixed $data  given data
+     * @param array $shape the shape of the data
+     *
+     * @return mixed
      */
     public static function getTranspose($data, array $shape)
     {
@@ -28,14 +41,21 @@ class Transpose
     }
 
     /**
-     * @param $data
-     * @param $shape
-     * @param array $indexes
-     * @param null $currentIndex
-     * @return array|mixed|\NumPHP\Core\NumArray
+     * Creates a transposed value or array recursive
+     *
+     * @param mixed $data         given data
+     * @param array $shape        the current shape
+     * @param array $indexes      given indexes
+     * @param int   $currentIndex current index
+     *
+     * @return mixed
      */
-    protected static function getTransposeRecursive($data, $shape, array $indexes = array(), $currentIndex = null)
-    {
+    protected static function getTransposeRecursive(
+        $data,
+        array $shape,
+        array $indexes = array(),
+        $currentIndex = null
+    ) {
         if (!is_null($currentIndex)) {
             $indexes[] = $currentIndex;
         }
@@ -43,7 +63,12 @@ class Transpose
             $transpose = [];
             $axis = array_pop($shape);
             for ($i = 0; $i < $axis; $i++) {
-                $transpose[] = self::getTransposeRecursive($data, $shape, $indexes, $i);
+                $transpose[] = self::getTransposeRecursive(
+                    $data,
+                    $shape,
+                    $indexes,
+                    $i
+                );
             }
             return $transpose;
         }
