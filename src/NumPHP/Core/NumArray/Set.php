387120334a256f46ceab446db49a31525c8070c6
Gordon Lesti <info@gordonlesti.com>
2015-01-08 14:46:48 +0100

    phpcs

--- src/NumPHP/Core/NumArray/Set.php
+++ src/NumPHP/Core/NumArray/Set.php
@@ -2,23 +2,36 @@
 /**
  * NumPHP (http://numphp.org/)
  *
- * @link http://github.com/GordonLesti/NumPHP for the canonical source repository
- * @copyright Copyright (c) 2014 Gordon Lesti (http://gordonlesti.com/)
- * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * PHP version 5
+ *
+ * @category  Core
+ * @package   NumPHP\Core\NumArray
+ * @author    Gordon Lesti <info@gordonlesti.com>
+ * @copyright 2014-2015 Gordon Lesti (https://gordonlesti.com/)
+ * @license   http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link      http://numphp.org/
  */
 
 namespace NumPHP\Core\NumArray;
 
 /**
  * Class Set
- * @package NumPHP\Core\NumArray
+ *
+ * @category Core
+ * @package  NumPHP\Core\NumArray
+ * @author   Gordon Lesti <info@gordonlesti.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     http://numphp.org/
  */
 class Set
 {
     /**
-     * @param $origData
-     * @param $subData
-     * @param array $args
+     * Replaces a value or sub array at the given position
+     *
+     * @param mixed $origData given data
+     * @param mixed $subData  replacing data
+     * @param array $args     array of indices
+     *
      * @return mixed
      */
     public static function setSubArray($origData, $subData, array $args)
@@ -27,16 +40,23 @@ class Set
     }
 
     /**
-     * @param $origData
-     * @param $subData
-     * @param $indexes
+     * Replaces a value or sub array at the given position recursive
+     *
+     * @param mixed $origData given data
+     * @param mixed $subData  replacing data
+     * @param array $indexes  array of indices
+     *
      * @return mixed
      */
-    protected static function setRecursive($origData, $subData, $indexes)
+    protected static function setRecursive($origData, $subData, array $indexes)
     {
         if (count($indexes)) {
             $index = array_shift($indexes);
-            $origData[$index] = self::setRecursive($origData[$index], $subData, $indexes);
+            $origData[$index] = self::setRecursive(
+                $origData[$index],
+                $subData,
+                $indexes
+            );
             return $origData;
         }
         return $subData;
