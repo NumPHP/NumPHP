2d97178c1c49949272480402da69caa3acc9cee6
Gordon Lesti <info@gordonlesti.com>
2015-01-08 13:04:08 +0100

    phpcs

--- src/NumPHP/Core/Exception/InvalidArgumentException.php
+++ src/NumPHP/Core/Exception/InvalidArgumentException.php
@@ -2,13 +2,27 @@
 /**
  * NumPHP (http://numphp.org/)
  *
- * @link http://github.com/GordonLesti/NumPHP for the canonical source repository
- * @copyright Copyright (c) 2014 Gordon Lesti (http://gordonlesti.com/)
- * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * PHP version 5
+ *
+ * @category  Core
+ * @package   NumPHP\Core\Exception
+ * @author    Gordon Lesti <info@gordonlesti.com>
+ * @copyright 2014-2015 Gordon Lesti (https://gordonlesti.com/)
+ * @license   http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link      http://numphp.org/
  */
 
 namespace NumPHP\Core\Exception;
 
+/**
+ * Class InvalidArgumentException
+ * 
+ * @category Core
+ * @package  NumPHP\Core\Exception
+ * @author   Gordon Lesti <info@gordonlesti.com>
+ * @license  http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link     http://numphp.org/
+ */
 class InvalidArgumentException extends \Exception
 {
 }
